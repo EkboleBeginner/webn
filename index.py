c9f1c22d77fb1beeaaadefc8922bb5e309e8bf43
EkboleBeginner <39627075+EkboleBeginner@users.noreply.github.com>
2018-06-24 04:48:10 +0000

    #!C:\Users\Administrator\AppData\Local\Programs\Python\Python36-32\python.exe version test

---
+++ index.py
@@ -0,0 +1,80 @@
+#!C:\Users\Administrator\AppData\Local\Programs\Python\Python36-32\python.exe
+print("Content-Type:text/html; charset=UTF-8\n")
+
+#docstring
+print('''
+<!DOCTYPE html>
+<html>
+<head>
+  <title> WEB1-WELCOME</title>
+  <meta charset="utf-8">
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+  <script async src="https://www.googletagmanager.com/gtag/js?id=UA-121211788-1"></script>
+  <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-121211788-1');
+  </script>
+</head>
+
+<body>
+  <h1><a href="index.html" title="WEB"> WEB </a></h1>
+<ol>
+  <li><a href="1.html" title="HTML"> HTML</a></li>
+  <li><a href="2.html" title="CSS"> CSS <a/></li>
+  <li><a href="3.html" title="JavaScript" > JavaScript</a></li>
+</ol>
+
+<h2>World Wide Web</h2>
+<p>
+  From Wikipedia, the free encyclopedia
+  (Redirected from WWW)
+  Jump to navigation
+  Jump to search
+  "WWW" and "The Web" redirect here. For other uses of WWW, see WWW (disambiguation). For other uses of web, see Web (disambiguation).
+  For the first web software, see WorldWideWeb.
+  Not to be confused with the Internet.The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and accessible via the Internet.[1] English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser in 1990 while employed at CERN in Switzerland.[2][3] The browser was released outside CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991.
+</P>
+<p>
+  <div id="disqus_thread"></div>
+<script>
+
+/**
+*  RECOMMENDED CONFIGURATION VARIABLES: EDIT AND UNCOMMENT THE SECTION BELOW TO INSERT DYNAMIC VALUES FROM YOUR PLATFORM OR CMS.
+*  LEARN WHY DEFINING THESE VARIABLES IS IMPORTANT: https://disqus.com/admin/universalcode/#configuration-variables*/
+/*
+var disqus_config = function () {
+this.page.url = PAGE_URL;  // Replace PAGE_URL with your page's canonical URL variable
+this.page.identifier = PAGE_IDENTIFIER; // Replace PAGE_IDENTIFIER with your page's unique identifier variable
+};
+*/
+(function() { // DON'T EDIT BELOW THIS LINE
+var d = document, s = d.createElement('script');
+s.src = 'https://web1-xz8top85m1.disqus.com/embed.js';
+s.setAttribute('data-timestamp', +new Date());
+(d.head || d.body).appendChild(s);
+})();
+</script>
+<noscript>Please enable JavaScript to view the <a href="https://disqus.com/?ref_noscript">comments powered by Disqus.</a></noscript>
+</p>
+<p>
+  <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5b0d57a010b99c7b36d46bdc/default';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+</p>
+
+
+  <body>
+</html>''')
